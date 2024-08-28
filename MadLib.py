9f8d864a64eb3ef5a76dd9adae7bd5c0a6ec9ec9
aaronspicka <aspicka@outlook.com>
2024-08-28 17:12:35 +0000

    final madlib draft

--- MadLib.py
+++ MadLib.py
@@ -6,14 +6,14 @@
 def main():
   print("Madlib")
   #Ask user for words
-  noun1 = input("enter a noun")
-  name1 = input("enter a noun")
-  name2 = input("enter a name")
-  noun2 = input("enter a noun")
-  noun3 = input("enter a noun")
-  noun4 = input("enter a noun")
+  noun1 = input("enter a noun: ")
+  name1 = input("enter a name: ")
+  name2 = input("enter a name: ")
+  noun2 = input("enter a noun: ")
+  noun3 = input("enter a noun: ")
+  noun4 = input("enter a noun: ")
   #Print the story with the user supplied words.
-  print("Me and my friends" + name1 + "and" + name2 + "drove to" +noun2 + "in" + noun1 + "but" + name1 + "forgot his" + noun3 + "in his" + noun4) 
+  print("Me and my friends " + name1 + " and " + name2 + " drove to " + noun2 + " in a " + noun1 + " but " + name1 + " forgot his " + noun3 + " in his " + noun4) 
   
   
 #Call the main function if this is the file being run.
