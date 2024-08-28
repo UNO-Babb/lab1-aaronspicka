67df2eb1050a61ad9cfc0ac743407da07ca6f19d
aaronspicka <aspicka@outlook.com>
2024-08-28 16:42:07 +0000

    madlib draft

--- MadLib.py
+++ MadLib.py
@@ -1,15 +1,20 @@
 #MadLib.py
-#Name:
-#Date:
+#Name:Aaron Spicka
+#Date:8/28/24
 #Assignment:
 
 def main():
   print("Madlib")
   #Ask user for words
-
+  noun1 = input("truck")
+  name1 = input("camdin")
+  name2 = input("jace")
+  noun2 = input("class")
+  noun3 = input("backpack")
+  noun4 = input("dorm")
   #Print the story with the user supplied words.
-
-
+  print("Me and my friends" + name1 + "and" + name2 + "drove to" +noun2 + "in" + noun1 + "but" + name1 + "forgot his" + noun3 + "in his" + noun4) 
+  
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
