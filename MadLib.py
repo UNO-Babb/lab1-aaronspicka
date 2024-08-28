00dc230b3947bd68c1e50eb5f0b8bcbc86446caa
aaronspicka <aspicka@outlook.com>
2024-08-28 16:47:06 +0000

    fixed input

--- MadLib.py
+++ MadLib.py
@@ -6,16 +6,16 @@
 def main():
   print("Madlib")
   #Ask user for words
-  noun1 = input("truck")
-  name1 = input("camdin")
-  name2 = input("jace")
-  noun2 = input("class")
-  noun3 = input("backpack")
-  noun4 = input("dorm")
+  noun1 = input("enter a noun")
+  name1 = input("enter a noun")
+  name2 = input("enter a name")
+  noun2 = input("enter a noun")
+  noun3 = input("enter a noun")
+  noun4 = input("enter a noun")
   #Print the story with the user supplied words.
   print("Me and my friends" + name1 + "and" + name2 + "drove to" +noun2 + "in" + noun1 + "but" + name1 + "forgot his" + noun3 + "in his" + noun4) 
   
-
+  
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
